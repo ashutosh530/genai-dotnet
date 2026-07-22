a02af342115e8b81db075984fafc7b62f8f33ab1
Ashutosh Singh <var4ashu@gmail.com>
2026-07-22 17:46:04 +0530

    Intial commit delegate

---
+++ Delegates/Delegate.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DelegateSystem
+{
+    class Delegate
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello, Ashutosh");
+        }
+    }
+
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using DelegateSystem;
+
+
+class Program
+{
+    public delegate void MyDelegate();
+    public delegate int VarDelegate(int a, int b);
+    static void SayHello()
+    {
+        Console.WriteLine("Hello, Ashutosh");
+    }
+    static void Intro()
+    {
+        Console.WriteLine("I am Ashutosh Singh");
+    }
+
+    // ReturnDelegate
+    static int Add(int a, int b) => (a+b);
+    static int Multiply(int a, int b) => (a*b);
+    public static void Main(string[] arg)
+    {
+        MyDelegate myDelegate = SayHello;
+        MyDelegate intro = Intro;
+        myDelegate();
+        intro();
+
+        // ReturnDelegate
+
+        VarDelegate var = Add;
+        Console.WriteLine("Addition : " + var(3,6));
+        var = Multiply;
+        Console.WriteLine("Multiply : " + var (5,6));
+
+    }
+}
